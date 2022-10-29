bca217ee7dfa4a75bb43b1aafb4e2c955e6f8919
schufeli <me@schufi.codes>
2022-10-29 14:19:13 +0200

    Sync commit

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,7 +1,32 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import { io } from "socket.io-client"
 
+const socket = io("http://192.168.0.100:5000");
+
+const velocity = ref(0);
+const rpm = ref(0);
+
+socket.on("currentVelocity", (data) => {
+  console.log(data)
+  velocity.value = Number(data);
+});
+
+socket.on("currentRPM", (data) => {
+  rpm.value = Number(data);
+});
 </script>
 
 <template>
-  <h1 class="font-extrabold underline">test</h1>
+  <section class="h-screen">
+    <div class="h-full grid grid-cols-2">
+      <div class="flex justify-center items-center bg-custom-100">
+        <h1 class="font-extrabold text-3xl">{{ velocity }} km/h</h1>
+      </div>
+
+      <div class="flex justify-center items-center bg-custom-200">
+        <h1 class="font-extrabold text-3xl">{{ rpm }} rpm</h1>
+      </div>
+    </div>
+  </section>
 </template>
--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -5,7 +5,15 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        custom: {
+          100: "#C7CBCD",
+          200: "#EEEDED",
+          300: "#A88C7B"
+        }
+      }
+    },
   },
   plugins: [],
 }
